b66e67ce625421ee4fa4f35eed77eeb4aff52bab
Aleksejs Kovalovs <aleks4444@inbox.lv>
2026-01-25 09:20:14 +0200

    More optimisations

--- ui/src/pages/GiftListsPage.test.tsx
+++ ui/src/pages/GiftListsPage.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@ari/plugin-sdk/test'
 import { describe, expect, it, vi } from 'vitest'
 
 import GiftListsPage from './GiftListsPage'
