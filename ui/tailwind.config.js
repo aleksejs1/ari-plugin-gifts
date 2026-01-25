f79cb7e50c012f796f7b624eab8dc15249b2d180
Aleksejs Kovalovs <aleks4444@inbox.lv>
2026-01-25 09:33:05 +0200

    More opt

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    presets: [require('../../../sdk/tailwind.preset.js')],
+import sharedPreset from '@ari/plugin-sdk/tailwind.preset'
+
+export default {
+    presets: [sharedPreset],
     content: ['./src/**/*.{ts,tsx}'],
 }
